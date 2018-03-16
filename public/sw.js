84cc8121056c15d99b65de79511707e7829b2fb5
Azrin Amin <azrin640@gmail.com>
2018-03-16 08:25:53 +0800

    Added full height support

--- public/sw.js
+++ public/sw.js
@@ -8,6 +8,4 @@ self.addEventListener('activate', function(event){
 });
 
 self.addEventListener('fetch', function(event){
-  console.log('[Service Worker] Fetching something ...', event);
-  event.respondWith(fetch(event.request));
 });
